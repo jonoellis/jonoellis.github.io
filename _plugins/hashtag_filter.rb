620f61c0be7962ec6a1cfd0b9d3b0ead352bb8c0
Jono Ellis <jono.ellis@gmail.com>
2025-02-11 20:36:45 +0000

    Update hashtag_filter.rb

--- _plugins/hashtag_filter.rb
+++ _plugins/hashtag_filter.rb
@@ -1,9 +1,7 @@
 module Jekyll
   module HashtagFilter
-    def link_hashtags(input)
-      input.gsub(/#(\w+)/) do |match|
-        "<a href='/discover.html?q=%23#{$1}' class='hashtag'>#{match}</a>"
-      end
+    def hashtags(input)
+      input.gsub(/#(\w+)/, '<a href="https://ellis.scot/discover.html?q=%23\1">#\1</a>')
     end
   end
 end
