f5997211b12330177a3fd1a3c162e0b4eb9884d9
Jono Ellis <jono.ellis@gmail.com>
2025-02-11 21:10:29 +0000

    Delete _plugins directory

--- _plugins/hashtag_filter.rb
+++
@@ -1,9 +0,0 @@
-module Jekyll
-  module HashtagFilter
-    def hashtags(input)
-      input.gsub(/#(\w+)/, '<a href="https://ellis.scot/discover.html?q=%23\1">#\1</a>')
-    end
-  end
-end
-
-Liquid::Template.register_filter(Jekyll::HashtagFilter)
